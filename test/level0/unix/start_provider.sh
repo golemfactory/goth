2f3f9d35b3468bb3f6b80a70e51dcb55def13869
azawlocki <artur.zawlocki@imapp.pl>
2020-04-10 16:34:59 +0200

    Change market API host/port from 127.0.0.1:5001 to 34.244.4.185:8080

--- test/level0/unix/start_provider.sh
+++ test/level0/unix/start_provider.sh
@@ -4,14 +4,16 @@
 NODE_ID=$(./with_env.sh provider.env ./get_node_id.sh)
 APP_KEY=$(./with_env.sh provider.env ./get_app_key.sh)
 
-curl -X POST "http://localhost:5001/admin/import-key" \
-     -H "accept: application/json" \
-     -H "Content-Type: application/json-patch+json" \
-     -d "[ { \"key\": \"${APP_KEY}\", \"nodeId\": \"${NODE_ID}\" }]"
-
 set -o allexport
 . ./provider.env
 set +o allexport
+
+# This is to make sure app keys are exported, since
+# right now we cannot rely on the daemon doing this
+curl -X POST "${MARKET_URL_BASE}/admin/import-key" \
+     -H "accept: application/json" \
+     -H "Content-Type: application/json-patch+json" \
+     -d "[ { \"key\": \"${APP_KEY}\", \"nodeId\": \"${NODE_ID}\" }]"
 
 export RUST_LOG=info
 ya-provider --activity-url "$YAGNA_ACTIVITY_URL" \
--- test/level0/unix/start_requestor.sh
+++ test/level0/unix/start_requestor.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+# Start the requestor agent
+
 NODE_ID=$(./with_env.sh requestor.env get_node_id.sh)
 APP_KEY=$(./with_env.sh requestor.env get_app_key.sh)
-
-curl -X POST "http://localhost:5001/admin/import-key" \
-     -H "accept: application/json" \
-     -H "Content-Type: application/json-patch+json" \
-     -d "[ { \"key\": \"${APP_KEY}\", \"nodeId\": \"${NODE_ID}\" }]"
 
 set -o allexport
 . ./requestor.env
 set +o allexport
+
+# This is to make sure app keys are exported, since
+# right now we cannot rely on the daemon doing this
+curl -X POST "${MARKET_URL_BASE}/admin/import-key" \
+     -H "accept: application/json" \
+     -H "Content-Type: application/json-patch+json" \
+     -d "[ { \"key\": \"${APP_KEY}\", \"nodeId\": \"${NODE_ID}\" }]"
 
 export RUST_LOG=info
 ya-requestor --activity-url "$YAGNA_ACTIVITY_URL" \
