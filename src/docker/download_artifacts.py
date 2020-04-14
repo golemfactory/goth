a206ce0484794a8e4a1cbe3441451996682d6c0c
Kuba Mazurek <zakaprov@gmail.com>
2020-04-14 18:16:36 +0200

    Even more reformatting

--- src/docker/download_artifacts.py
+++ src/docker/download_artifacts.py
@@ -10,7 +10,8 @@ import typing
 import requests
 
 logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(name)-35s %(message)s", level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(name)-35s %(message)s",
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
@@ -57,7 +58,8 @@ def get_latest_run(workflow_id: str) -> dict:
     logger.debug("workflow_runs=%s", workflow_runs)
     result = next(
         filter(
-            lambda r: r["conclusion"] == "success" and r["head_branch"] == BRANCH,
+            lambda r: r["conclusion"] == "success"
+            and r["head_branch"] == BRANCH,
             workflow_runs,
         )
     )
