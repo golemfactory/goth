0acb54a01af8db18c547b97b4b276deb325bce1f
azawlocki <artur.zawlocki@imapp.pl>
2020-04-21 00:21:55 +0200

    Adjust `start_provider.sh` to changes in `ya-provider` command

--- test/level0/unix/start_provider.sh
+++ test/level0/unix/start_provider.sh
@@ -8,15 +8,23 @@ set -o allexport
 . ./provider.env
 set +o allexport
 
-# This is to make sure app keys are exported, since
-# right now we cannot rely on the daemon doing this
-curl -X POST "${MARKET_URL_BASE}/admin/import-key" \
-     -H "accept: application/json" \
-     -H "Content-Type: application/json-patch+json" \
-     -d "[ { \"key\": \"${APP_KEY}\", \"nodeId\": \"${NODE_ID}\" }]"
+# The file `presets.json` is required in the current dir.
+[ -f "./presets.json" ] || cp ../asset/presets.json .
+
+# This preset must be defined in `presets.json`:
+PRESET_NAME=amazing-offer
+
+# This is the standard location of `exeunits-descriptor.json`
+# for `ya-provider` installed from `.deb` package:
+EXE_UNIT_PATH="/usr/lib/yagna/plugins/exeunits-descriptor.json"
 
 export RUST_LOG=info
-ya-provider --activity-url "$YAGNA_ACTIVITY_URL" \
+
+ya-provider --exe-unit-path "$EXE_UNIT_PATH" \
+	    run \
+	    --activity-url "$YAGNA_ACTIVITY_URL" \
 	    --app-key "$APP_KEY" \
 	    --credit-address "$NODE_ID" \
-	    --market-url "$YAGNA_MARKET_URL"
+	    --market-url "$YAGNA_MARKET_URL" \
+	    --node-name test-provider \
+	    "$PRESET_NAME"
