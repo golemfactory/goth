7bdfbc8b1b7348bb7dd3c93dd28db8b0525fe7f9
azawlocki <artur.zawlocki@imapp.pl>
2020-04-24 19:16:05 +0200

    Modify test scripts to optionally run using proxy ports

--- test/level0/unix/start_net_mk1_hub.sh
+++ test/level0/unix/start_net_mk1_hub.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export RUST_LOG=info
 ya_sb_router -l tcp://127.0.0.1:7477
--- test/level0/unix/start_requestor.sh
+++ test/level0/unix/start_requestor.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # Start the requestor agent
+export AGENT=1
 
 NODE_ID=$(./with_env.sh requestor.env get_node_id.sh)
 APP_KEY=$(./with_env.sh requestor.env get_app_key.sh)
 
+echo "Node ID: $NODE_ID"
+echo "App key: $APP_KEY"
+
 set -o allexport
 . ./requestor.env
 set +o allexport
+
+echo "YAGNA_ACTIVITY_URL: $YAGNA_ACTIVITY_URL" > /dev/stderr
+echo "YAGNA_MARKET_URL: $YAGNA_MARKET_URL" > /dev/stderr
 
 export RUST_LOG=info
 
