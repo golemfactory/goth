b1700e4f616fcab2cf8306b06e3a8b4e43d4d9bf
azawlocki <artur.zawlocki@golem.network>
2020-05-14 10:09:20 +0200

    Adjust add-on to coroutine implementation of temporal assertions

--- src/api-tracer/tracer_addon.py
+++ src/api-tracer/tracer_addon.py
@@ -3,6 +3,7 @@ Mitmproxy addon that traces API calls and verifies
 that the sequence of calls satifies given properties
 """
 from __future__ import annotations
+import asyncio
 import importlib
 import logging
 import queue
@@ -13,7 +14,9 @@ import mitmproxy
 from mitmproxy.flow import Error
 from mitmproxy.http import HTTPFlow, HTTPRequest, HTTPResponse
 
-from router_addon import CALLER_HEADER, CALLEE_HEADER
+from api_events import APICall, APIError, APIEvent, APIResult
+from assertions import Assertion
+from assertions import logger as assertions_logger
 
 
 logging.basicConfig(
@@ -36,6 +39,7 @@ call_logger.handlers = [_log_handler]
 call_logger.propagate = False
 
 
+<<<<<<< HEAD
 class APICall:
     """
     Represents an API call, consisting of a HTTP Request and an optional
@@ -100,6 +104,9 @@ def trace_property(func: Property):
 
 
 class CallTrace:
+=======
+class APIMonitor:
+>>>>>>> Adjust add-on to coroutine implementation of temporal assertions
     """
     Represents a sequence of API calls and a set of properties
     that the sequence has to satisfy.
@@ -119,19 +126,34 @@ class CallTrace:
     # All heavy work is done in a separate thread
     worker: threading.Thread
 
+<<<<<<< HEAD
     # List of properties that have to be checked for each new call
     # Each property is stored with a label that is used in log messages
     properties: List[Property]
+=======
+    # List of assertion functions to be instantiated by the worker thread
+    assertion_funcs: List[Callable]  # Assertion[APIEvent]]
+>>>>>>> Adjust add-on to coroutine implementation of temporal assertions
 
     def __init__(self):
         self.calls = []
         self.incoming = queue.Queue()
+<<<<<<< HEAD
         self.properties = []
         self.worker = threading.Thread(target=self._run, daemon=True)
+=======
+        self.assertion_funcs = []
+        self.worker = threading.Thread(
+            target=lambda: asyncio.run(self._run_worker()),
+            name="AssertionsThread",
+            daemon=True
+        )
+>>>>>>> Adjust add-on to coroutine implementation of temporal assertions
 
     def load_properties(self, module_name: str) -> None:
         """Load property functions from a module"""
         mod = importlib.import_module(module_name)
+<<<<<<< HEAD
         prev_loaded = len(self.properties)
         for _, elem in mod.__dict__.items():
             # This test is lame, but simply checking if `type(elem)`
@@ -148,6 +170,9 @@ class CallTrace:
             len(self.properties) - prev_loaded,
             module_name,
         )
+=======
+        self.assertion_funcs.extend(mod.__dict__["TEMPORAL_ASSERTIONS"])
+>>>>>>> Adjust add-on to coroutine implementation of temporal assertions
 
     def start(self) -> None:
         """Start tracing API calls"""
@@ -166,17 +191,36 @@ class CallTrace:
         """Return the number of registered calls"""
         return len(self.calls)
 
-    def _run(self) -> None:
+    def _instantiate_assertions(self) -> List[Assertion[APIEvent]]:
+        """Create assertion objects from assertion functions.
+
+        Note: this must be done in the same thread that runs the asyncio loop.
+        """
+        assertions = []
+        for func in self.assertion_funcs:
+            a = Assertion(self.events, func)
+            logger.debug("Created assertion '%s'", a.name)
+            assertions.append(a)
+
+        return assertions
+
+    async def _run_worker(self) -> None:
         """
         Run the thread that adds incoming requests/responses/errors
         to the trace and checks the properties
         """
+        logger.info("Assertions thread started")
+
+        assertions = self._instantiate_assertions()
+
+        # Run the main assertions loop
         while True:
             flow = self.incoming.get()
             if flow is None:
                 # it's time to finish
                 break
 
+<<<<<<< HEAD
             num, call = self._find_or_create_call(flow)
 
             if flow.error or flow.response:
@@ -225,7 +269,62 @@ class CallTrace:
             if not prop.check(self.calls):
                 index = len(self.calls)
                 logger.warning("Property '%s' failed at %d", prop.label, index)
+=======
+            self.events.append(event)
+            self._log_event(event)
+            assertions = await self._check_assertions(assertions)
 
+        # End of history reached! Check the remaining assertions.
+        await self._check_assertions(assertions, at_end=True)
+
+    def _log_event(self, event: APIEvent) -> None:
+        if isinstance(event, APICall):
+            status = "in progress"
+            call = event
+        elif isinstance(event, APIResult):
+            status = f"completed ({event.response.status_code})"
+            call = event.call
+        elif isinstance(event, APIError):
+            status = "failed"
+            call = event.call
+
+        logger.info("%s:\t%s", call, status)
+
+        call_logger.info(
+            "%s:\t%s",
+            event,
+            status,
+            extra={
+                "num": call.number,
+                "caller": call.caller,
+                "callee": call.callee,
+                "method": call.request.method,
+                "path": call.request.path,
+                "status": status,
+            },
+        )
+
+    async def _check_assertions(
+        self,
+        assertions: List[Assertion[APIEvent]],
+        at_end: bool = False
+    ) -> None:
+
+        active: List[Assertion[APIEvent]] = []
+        event_descr = f"event #{len(self.events)}" if not at_end else "all events"
+
+        for a in assertions:
+            await a.process_event()
+            if a.accepted:
+                assertions_logger.debug("Satisfied after %s: %s", event_descr, a.name)
+            elif a.failed:
+                assertions_logger.debug("Failed after %s: %s", event_descr, a.name)
+                raise AssertionError(str(a))
+            else:
+                active.append(a)
+>>>>>>> Adjust add-on to coroutine implementation of temporal assertions
+
+        return active
 
 class TracerAddon:
     """This add-on keeps track of API calls"""
