feb3239e59d4b9c9f634adf9308da67f0be3fe4d
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-05-19 13:42:50 +0200

    Manually apply polymorphism to Demand, Proposal and the ProposalEvent reply

--- model/market/openapi_market_client/api/requestor_api.py
+++ model/market/openapi_market_client/api/requestor_api.py
@@ -270,7 +270,7 @@ class RequestorApi(object):
             body=body_params,
             post_params=form_params,
             files=local_var_files,
-            response_type='list[OneOfProposalEventPropertyQueryEvent]',  # noqa: E501
+            response_type='list[ProposalEvent]',  # noqa: E501
             auth_settings=auth_settings,
             async_req=local_var_params.get('async_req'),
             _return_http_data_only=local_var_params.get('_return_http_data_only'),  # noqa: E501
