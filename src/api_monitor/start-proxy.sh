f75c7e6103cf961af7e45111b7c6e154962b1e63
azawlocki <artur.zawlocki@golem.network>
2020-06-04 18:47:52 +0200

    Fix `trap` statement in `start-proxy.sh`

--- src/api_monitor/start-proxy.sh
+++ src/api_monitor/start-proxy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+
 echo "Starting nginx..."
 nginx -p . -c ./nginx.conf &
-trap "nginx -p . -c ./nginx.conf -s stop" KILL
-
-SCRIPT_DIR=$(dirname $0)
-export PYTHONPATH=$SCRIPT_DIR/../..:$PYTHONPATH
+trap "[ ! -e ./nginx.pid ] || nginx -p . -c ./nginx.conf -s stop" EXIT
 
 echo "Starting mitmproxy..."
+export PYTHONPATH=../..:$PYTHONPATH
 mitmdump --mode reverse:"http://127.0.0.1" \
     --listen-port 9000 \
     -q \
