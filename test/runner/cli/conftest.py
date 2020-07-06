98224091bc4ee186936d7fcfa7156e18aba31ab0
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2020-07-06 19:28:59 +0200

    Refactor MockYagnaCLI, update integration.yml

--- test/runner/cli/conftest.py
+++ test/runner/cli/conftest.py
@@ -1,23 +1,13 @@
 """Code common for all pytest modules in this package"""
-from unittest.mock import MagicMock
 
 from docker import DockerClient
 from docker.models.containers import Container
-import json
-import ipaddress
 import pytest
-import re
-import random
-import shlex
-import socket
-import struct
-import subprocess
+from unittest.mock import MagicMock
 
+from .mock import MockYagnaCLI
 from goth.runner.container import DockerContainer
 from goth.runner.container.yagna import YagnaContainer, YagnaContainerConfig
-from goth.runner.exceptions import CommandError, KeyAlreadyExistsError
-from goth.runner.log import LogConfig
-from goth.runner.probe import Role
 
 GENERIC_COMMAND = ["cmd_name", "-f", "flag_value"]
 GENERIC_ENTRYPOINT = "/usr/bin/binary_name"
@@ -25,196 +15,17 @@ GENERIC_IMAGE = "some_docker_image"
 GENERIC_NAME = "generic_container"
 YAGNA_CONTAINER_NAME = "yagna_container"
 
-key_list = {"headers": ["name", "key", "id", "role", "created"], "values": []}
 
-key_values = []
-
-id_list = {"headers": ["alias", "default", "locked", "address"], "values": []}
-
-id_values = []
-
-payment_status_value = {
-    "amount": 0,
-    "incoming": {"accepted": 0, "confirmed": 0, "rejected": 0, "requested": 0},
-    "outgoing": {"accepted": 0, "confirmed": 0, "rejected": 0, "requested": 0},
-    "reserved": 0,
-}
-
-
-def echo_demux(_options, cmd):
-    command = re.search(r"\'(.*?)\'", cmd).group(1)
-    proc = subprocess.Popen(
-        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    proc.wait()
-    result = proc.communicate()
-    if proc.returncode != 0:
-        raise CommandError("no-such-command")
-
-    return result
-
-
-def create_app_key(options, cmd):
-    alias = options.get("--id")
-    key = re.search(r"^.*?\bcreate \'(.*)\'(?:.+)?$", cmd).group(1)
-
-    if isinstance(alias, str) and not is_item_in(id_values, alias, 0):
-        raise CommandError("no-such-command")
-
-    if is_item_in(key_values, key, 1):
-        raise KeyAlreadyExistsError(key)
-
-    key_values.append(["name", key, alias, "", ""])
-    key = f'"{key}"'
-    return (key.encode(), b"")
-
-
-def list_app_key(options, cmd):
-    key_id = options.get("--id")
-    alias = None
-
-    if is_ipv4(key_id):
-        result_alias = filter_list(id_values, key_id, 3)
-        alias = result_alias[0][0]
-        alias = alias if alias else key_id
-    else:
-        alias = key_id
-
-    key_value_list = filter_list(key_values, alias, 2) if key_id else key_values
-    key_list["values"] = key_value_list
-    return (json.dumps(key_list).encode("utf-8"), b"")
-
-
-def create_id(options, cmd):
-    alias = options.get("--no-password")
-    alias = alias if isinstance(alias, str) else None
-
-    if alias and is_item_in(id_values, alias, 0):
-        raise CommandError("no-such-command")
-
-    address = socket.inet_ntoa(struct.pack(">I", random.randint(1, 0xFFFFFFFF)))
-    id_values.append([alias, None, False, address])
-    result = {
-        "Ok": {
-            "alias": alias,
-            "isDefault": False,
-            "isLocked": False,
-            "nodeId": address,
-        }
-    }
-    return (json.dumps(result).encode("utf-8"), b"")
-
-
-def show_id(_options, cmd):
-    alias_or_addr = None
-    filter_index = 3
-    result_list = id_values
-    try:
-        alias_or_addr = re.search(r"^.*?\bshow ([^\s]+)", cmd).group(1)
-    except:
-        alias_or_addr = None
-
-    if alias_or_addr:
-        if not is_ipv4(alias_or_addr):
-            filter_index = 0
-        result_list = filter_list(id_values, alias_or_addr, filter_index)
-
-    if len(result_list) < 1:
-        result = {"Ok": None}
-    else:
-        [alias, is_default, is_locked, address] = result_list[0]
-        result = {
-            "Ok": {
-                "alias": alias,
-                "isDefault": True if is_default == "X" else False,
-                "isLocked": is_locked,
-                "nodeId": address,
-            }
-        }
-    return (json.dumps(result).encode("utf-8"), b"")
-
-
-def list_id(_options, _cmd):
-    id_list["values"] = id_values
-    return (json.dumps(id_list).encode("utf-8"), b"")
-
-
-def payment_status(_options, _cmd):
-    return (json.dumps(payment_status_value).encode("utf-8"), b"")
-
-
-commands = {
-    "/bin/sh": {"-c": echo_demux},
-    "yagna": {
-        "app-key": {"create": create_app_key, "list": list_app_key},
-        "id": {"create": create_id, "show": show_id, "list": list_id},
-        "payment": {"init": payment_status, "status": payment_status},
-    },
-}
-
-
-def is_ipv4(value):
-    try:
-        ipaddress.IPv4Network(value)
-        return True
-    except ValueError:
-        return False
-
-
-def filter_list(target_list, item, index):
-    return list(filter(lambda x: x[index] == item, target_list))
-
-
-def is_item_in(target_list, item, index):
-    result = filter_list(target_list, item, index)
-    return len(result) > 0
-
-
-def deep_get_fn(dictionary, keys):
-    if not keys or dictionary is None:
-        return dictionary
-
-    if callable(dictionary):
-        return dictionary
-
-    return deep_get_fn(dictionary.get(keys[0]), keys[1:])
-
-
-def call_fn(args, cmd, options):
-    fn = deep_get_fn(commands, args)
-    return fn(options, cmd)
-
-
-def reset_states():
-    key_values.clear()
-    id_values.clear()
-    id_values.append([None, "X", False, None])
+@pytest.fixture
+def mock_yagna_cli():
+    return MockYagnaCLI()
 
 
 @pytest.fixture
-def exec_run():
-    def _exec_run(cmd, demux):
-        cmd = cmd.replace("--json", "")
-        args = shlex.split(cmd)
-        options = {
-            k: True if v.startswith("-") else v
-            for k, v in zip(args, args[1:] + ["--"])
-            if k.startswith("-")
-        }
-        result_mock = MagicMock()
-        result_mock.exit_code = 0
-
-        result_mock.output = call_fn(args, cmd, options)
-        return result_mock
-
-    return _exec_run
-
-
-@pytest.fixture
-def mock_container(exec_run):
+def mock_container(mock_yagna_cli):
     mock_container = MagicMock(spec=Container)
     mock_container.status = "created"
-    mock_container.exec_run = exec_run
+    mock_container.exec_run = mock_yagna_cli.exec_run
     return mock_container
 
 
@@ -226,7 +37,7 @@ def mock_docker_client(mock_container):
 
 
 @pytest.fixture
-def docker_container(mock_docker_client):
+def docker_container(mock_docker_client, mock_container):
     mock_docker_client.containers.create.return_value = mock_container
     return DockerContainer(
         client=mock_docker_client,
@@ -240,7 +51,6 @@ def docker_container(mock_docker_client):
 @pytest.fixture
 def yagna_container(mock_docker_client):
     """A fixture for starting and terminating a container using the `yagna` image"""
-    reset_states()
     config = MagicMock(spec=YagnaContainerConfig)
     config.name = YAGNA_CONTAINER_NAME
     config.environment = {}
---
+++ test/runner/cli/mock.py
@@ -0,0 +1,221 @@
+from dataclasses import asdict, astuple, fields
+import json
+import ipaddress
+import re
+import random
+import shlex
+import socket
+import struct
+import subprocess
+from typing import List
+
+from unittest.mock import MagicMock
+
+from goth.runner.cli.yagna_app_key_cmd import AppKeyInfo
+from goth.runner.cli.yagna_id_cmd import Identity
+from goth.runner.cli.yagna_payment_cmd import Payments, PaymentStatus
+from goth.runner.exceptions import CommandError, KeyAlreadyExistsError
+
+class MockYagnaCLI:
+    commands: dict = {}
+    id_list = {"headers": ["alias", "default", "locked", "address"], "values": []}
+    id_values: List[Identity] = []
+    key_list = {"headers": ["name", "key", "id", "role", "created"], "values": []}
+    key_values: List[AppKeyInfo] = []
+    payment_status_value = PaymentStatus(
+        0, Payments(0, 0, 0, 0), Payments(0, 0, 0, 0), 0
+    )
+
+    def __init__(self):
+        self.commands = {
+            "/bin/sh": {"-c": self.echo_demux},
+            "yagna": {
+                "app-key": {"create": self.create_app_key, "list": self.list_app_key},
+                "id": {
+                    "create": self.create_id,
+                    "show": self.show_id,
+                    "list": self.list_id,
+                },
+                "payment": {"init": self.payment_status, "status": self.payment_status},
+            },
+        }
+        self.reset_states()
+        self.id_values.append(
+            Identity(alias="", is_default=True, is_locked=False, address="")
+        )
+
+    def is_ipv4(self, value):
+        """Check if given value is IPv4."""
+        try:
+            ipaddress.IPv4Network(value)
+            return True
+        except ValueError:
+            return False
+
+    def filter_list(self, target_list, item, index):
+        return list(filter(lambda x: getattr(x, index) == item, target_list))
+
+    def is_item_in(self, target_list, item, index):
+        result = self.filter_list(target_list, item, index)
+        return len(result) > 0
+
+    def deep_get_fn(self, dictionary, keys):
+        """Recursively search for function in nested object."""
+        if not keys or dictionary is None:
+            return dictionary
+
+        if callable(dictionary):
+            return dictionary
+
+        return self.deep_get_fn(dictionary.get(keys[0]), keys[1:])
+
+    def reset_states(self):
+        """Reset states for each test."""
+        self.key_values.clear()
+        self.id_values.clear()
+
+    def call_fn(self, args, cmd, options):
+        """Call mapped function from given commands."""
+        fn = self.deep_get_fn(self.commands, args)
+        return fn(options, cmd)
+
+    def parse_options(self, cmd_args):
+        """Parse flags from given commands"""
+        cmd_options = {}
+
+        """
+            Create key value pairs to catch flags and their values
+
+            i.e.
+            input: "yagna id create --no-password id-alias"
+            output: {('id-alias', '--'), ('id', 'create'), 
+            ('create', '--no-password'), 
+            ('--no-password', 'id-alias'), ('yagna', 'id')}
+        """
+        zip_pairs = zip(cmd_args, cmd_args[1:] + ["--"])
+        for key, value in zip_pairs:
+            if key.startswith("--"):
+                cmd_options[key] = True if value.startswith("-") else value
+        return cmd_options
+
+    def exec_run(self, cmd, **kwargs):
+        """Mock function of docker exec_run"""
+        cmd = cmd.replace("--json", "")
+        cmd_args = shlex.split(cmd)
+        cmd_options = self.parse_options(cmd_args)
+
+        result_mock = MagicMock()
+        result_mock.exit_code = 0
+
+        result_mock.output = self.call_fn(cmd_args, cmd, cmd_options)
+        return result_mock
+
+    def echo_demux(self, _options, cmd):
+        """Mock function of command line interface of yagna docker container"""
+        command = re.search(r"\'(.*?)\'", cmd).group(1)
+        proc = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        proc.wait()
+        result = proc.communicate()
+        if proc.returncode != 0:
+            raise CommandError("no-such-command")
+
+        return result
+
+    def create_app_key(self, options, cmd):
+        """Creates yagna app key."""
+        alias = options.get("--id")
+        key = re.search(r"^.*?\bcreate \'(.*)\'(?:.+)?$", cmd).group(1)
+
+        if isinstance(alias, str) and not self.is_item_in(
+            self.id_values, alias, "alias"
+        ):
+            raise CommandError("no-such-command")
+
+        if self.is_item_in(self.key_values, key, "key"):
+            raise KeyAlreadyExistsError(key)
+
+        self.key_values.append(AppKeyInfo("name", key, alias, "", ""))
+        key = f'"{key}"'
+        return (key.encode(), b"")
+
+    def list_app_key(self, options, cmd):
+        """Lists current yagna app keys."""
+        key_id = options.get("--id")
+        alias = None
+
+        if self.is_ipv4(key_id):
+            result_alias = self.filter_list(self.id_values, key_id, "address")
+            alias = result_alias[0].alias
+            alias = alias if alias else key_id
+        else:
+            alias = key_id
+
+        key_value_list = (
+            self.filter_list(self.key_values, alias, "address")
+            if key_id
+            else self.key_values
+        )
+        self.key_list["values"] = [list(ob.__dict__.values()) for ob in key_value_list]
+        return (json.dumps(self.key_list).encode("utf-8"), b"")
+
+    def create_id(self, options, cmd):
+        """Creates id"""
+        alias = options.get("--no-password")
+        alias = alias if isinstance(alias, str) else None
+
+        if alias and self.is_item_in(self.id_values, alias, "alias"):
+            raise CommandError("no-such-command")
+
+        address = socket.inet_ntoa(struct.pack(">I", random.randint(1, 0xFFFFFFFF)))
+        self.id_values.append(Identity(alias, False, False, address))
+        result = {
+            "Ok": {
+                "alias": alias,
+                "isDefault": False,
+                "isLocked": False,
+                "nodeId": address,
+            }
+        }
+        return (json.dumps(result).encode("utf-8"), b"")
+
+    def show_id(self, _options, cmd):
+        """Shows specific id if exist"""
+        filter_index = "address"
+        result_list = self.id_values
+        try:
+            alias_or_addr = re.search(r"^.*?\bshow ([^\s]+)", cmd).group(1)
+        except:
+            alias_or_addr = None
+
+        if alias_or_addr:
+            if not self.is_ipv4(alias_or_addr):
+                filter_index = "alias"
+            result_list = self.filter_list(self.id_values, alias_or_addr, filter_index)
+
+        if len(result_list) < 1:
+            result = {"Ok": None}
+        else:
+            [alias, is_default, is_locked, address] = astuple(result_list[0])
+            result = {
+                "Ok": {
+                    "alias": alias,
+                    "isDefault": is_default,
+                    "isLocked": is_locked,
+                    "nodeId": address,
+                }
+            }
+        return (json.dumps(result).encode("utf-8"), b"")
+
+    def list_id(self, _options, _cmd):
+        """Lists current ids"""
+        list_values = [list(ob.__dict__.values()) for ob in self.id_values]
+        for item in list_values:
+            item[1] = "X" if item[1] else None
+        self.id_list["values"] = list_values
+        return (json.dumps(self.id_list).encode("utf-8"), b"")
+
+    def payment_status(self, _options, _cmd):
+        """Shows payment statuses"""
+        return (json.dumps(asdict(self.payment_status_value)).encode("utf-8"), b"")
