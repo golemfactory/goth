7dbf5969090f6322e9101993ef64a62941257b54
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-07-24 12:46:11 +0200

    Run level 0 manual unix scenario with local docler-yagna-gnt2 setup

--- test/level0/unix/setup_node.sh
+++ test/level0/unix/setup_node.sh
@@ -22,9 +22,29 @@ trap "kill $!" EXIT
 echo "Wait until the daemon is ready to create keys"
 tail -f $DAEMON_LOG | grep -m1 'Market service successfully activated'
 
+yagna id list
+DEFAULT_KEY=$(sh get_node_id.sh)
+echo "Default key=${DEFAULT_KEY}"
+CREATE_KEY_RESULT=$(yagna id create --json --no-password --from-keystore ${YAGNA_KEY_FILE})
+echo "Default key=${CREATE_KEY_RESULT}"
+SCRIPT="\
+import json, sys; \
+j = json.load(sys.stdin); \
+print(j['Ok']['nodeId'])"
+
+
+NEW_KEY=$(echo ${CREATE_KEY_RESULT} | python3 -c "${SCRIPT}")
+yagna id update --set-default "${NEW_KEY}"
+yagna id list
+yagna id drop --help
+echo "Dropping key ${DEFAULT_KEY}"
+yagna id drop ${DEFAULT_KEY}
+yagna id list
+
 echo "Create app key"
 yagna app-key drop "$KEY_NAME"
 yagna app-key create "$KEY_NAME"
+yagna app-key list
 
 echo "Stop the daemon"
 sh ./stop_daemon.sh
