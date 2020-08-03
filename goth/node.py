fd9d47e8c9c745192e0bd40a2609fb60c7f4ef24
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-08-03 18:05:24 +0200

    Moved node_environment and VOLUMES to own file

---
+++ goth/node.py
@@ -0,0 +1,50 @@
+"""Helper class for yagna node environment variables and Volumes."""
+
+from string import Template
+from typing import Dict
+
+from goth.address import (
+    ACTIVITY_API_URL,
+    MARKET_API_URL,
+    PAYMENT_API_URL,
+    ROUTER_HOST,
+    ROUTER_PORT,
+    YAGNA_BUS_URL,
+    YAGNA_REST_URL,
+)
+
+VOLUMES = {
+    Template("$assets_path"): "/asset",
+    Template("$assets_path/presets.json"): "/presets.json",
+}
+
+
+def node_environment(
+    market_url_base: str = "", rest_api_url_base: str = ""
+) -> Dict[str, str]:
+    """Construct an environment for executing commands in a yagna docker container."""
+    # Use custom base if given, default otherwise
+    market_template_params = {"base": market_url_base} if market_url_base else {}
+
+    daemon_env = {
+        "CENTRAL_MARKET_URL": MARKET_API_URL.substitute(market_template_params),
+        "CENTRAL_NET_HOST": f"{ROUTER_HOST}:{ROUTER_PORT}",
+        "CHAIN": "mainnet",
+        "GETH_ADDRESS": "http://ethereum:8545",
+        "GNT2_CONTRACT_ADDRESS": "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34",
+        "GSB_URL": YAGNA_BUS_URL.substitute(host="0.0.0.0"),
+        "YAGNA_API_URL": YAGNA_REST_URL.substitute(host="0.0.0.0"),
+        "RUST_LOG": "debug,tokio_core=info,tokio_reactor=info,hyper=info",
+        "REQUIRED_CONFIRMATIONS": "1",
+    }
+    node_env = daemon_env
+
+    if rest_api_url_base:
+        agent_env = {
+            "YAGNA_MARKET_URL": MARKET_API_URL.substitute(base=rest_api_url_base),
+            "YAGNA_ACTIVITY_URL": ACTIVITY_API_URL.substitute(base=rest_api_url_base),
+            "YAGNA_PAYMENT_URL": PAYMENT_API_URL.substitute(base=rest_api_url_base),
+        }
+        node_env.update(agent_env)
+
+    return node_env
--- test/level1/test_level1.py
+++ test/level1/test_level1.py
@@ -2,65 +2,22 @@
 
 import logging
 from pathlib import Path
-from string import Template
-from typing import Dict, Optional
+from typing import Optional
 
 import pytest
 
 from goth.address import (
-    ACTIVITY_API_URL,
-    MARKET_API_URL,
     MARKET_BASE_URL,
-    PAYMENT_API_URL,
     PROXY_HOST,
-    ROUTER_HOST,
-    ROUTER_PORT,
-    YAGNA_BUS_URL,
     YAGNA_REST_URL,
 )
+from goth.node import node_environment, VOLUMES
 from goth.runner import Runner
 
 from goth.runner.container.yagna import YagnaContainerConfig
 from goth.runner.probe import Provider, Requestor
 
 logger = logging.getLogger(__name__)
-
-
-def node_environment(
-    market_url_base: str = "", rest_api_url_base: str = ""
-) -> Dict[str, str]:
-    """Construct an environment for executing commands in a yagna docker container."""
-    # Use custom base if given, default otherwise
-    market_template_params = {"base": market_url_base} if market_url_base else {}
-
-    daemon_env = {
-        "CENTRAL_MARKET_URL": MARKET_API_URL.substitute(market_template_params),
-        "CENTRAL_NET_HOST": f"{ROUTER_HOST}:{ROUTER_PORT}",
-        "CHAIN": "mainnet",
-        "GETH_ADDRESS": "http://ethereum:8545",
-        "GNT2_CONTRACT_ADDRESS": "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34",
-        "GSB_URL": YAGNA_BUS_URL.substitute(host="0.0.0.0"),
-        "YAGNA_API_URL": YAGNA_REST_URL.substitute(host="0.0.0.0"),
-        "RUST_LOG": "debug,tokio_core=info,tokio_reactor=info,hyper=info",
-    }
-    node_env = daemon_env
-
-    if rest_api_url_base:
-        agent_env = {
-            "YAGNA_MARKET_URL": MARKET_API_URL.substitute(base=rest_api_url_base),
-            "YAGNA_ACTIVITY_URL": ACTIVITY_API_URL.substitute(base=rest_api_url_base),
-            "YAGNA_PAYMENT_URL": PAYMENT_API_URL.substitute(base=rest_api_url_base),
-        }
-        node_env.update(agent_env)
-
-    return node_env
-
-
-VOLUMES = {
-    Template("$assets_path"): "/asset",
-    Template("$assets_path/presets.json"): "/presets.json",
-}
-
 
 LEVEL1_TOPOLOGY = [
     YagnaContainerConfig(
