b743af826315dbff598fa2a6bb920f7e52e82ec6
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-08-24 14:04:24 +0200

    Find / replace old market url for new market url

--- goth/runner/probe_steps.py
+++ goth/runner/probe_steps.py
@@ -201,7 +201,7 @@ class ProbeStepBuilder:
 
         package = (
             "hash://sha3:d5e31b2eed628572a5898bf8c34447644bfc4b5130cfc1e4f10aeaa1"
-            ":http://34.244.4.185:8000/rust-wasi-tutorial.zip"
+            ":http://3.249.139.167:8000/rust-wasi-tutorial.zip"
         )
         constraints = (
             "(&(golem.inf.mem.gib>0.5)(golem.inf.storage.gib>1)"
--- test/level0/unix/start_requestor.sh
+++ test/level0/unix/start_requestor.sh
@@ -24,4 +24,4 @@ ya-requestor --activity-url "$YAGNA_ACTIVITY_URL" \
        --app-key "$APP_KEY" \
        --exe-script "../asset/exe_script.json" \
        --market-url "$YAGNA_MARKET_URL" \
-       --task-package "hash://sha3:d5e31b2eed628572a5898bf8c34447644bfc4b5130cfc1e4f10aeaa1:http://34.244.4.185:8000/rust-wasi-tutorial.zip"
+       --task-package "hash://sha3:d5e31b2eed628572a5898bf8c34447644bfc4b5130cfc1e4f10aeaa1:http://3.249.139.167:8000/rust-wasi-tutorial.zip"
