f5f1fee5b22fb69bcca2cd3320c7bbf3165f3eee
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-08-24 14:49:45 +0200

    Updated NGNT contract address var name

--- goth/node.py
+++ goth/node.py
@@ -31,7 +31,7 @@ def node_environment(
         "CENTRAL_NET_HOST": f"{ROUTER_HOST}:{ROUTER_PORT}",
         "CHAIN": "mainnet",
         "GETH_ADDRESS": "http://ethereum:8545",
-        "GNT2_CONTRACT_ADDRESS": "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34",
+        "NGNT_CONTRACT_ADDRESS": "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34",
         "GSB_URL": YAGNA_BUS_URL.substitute(host="0.0.0.0"),
         "YAGNA_API_URL": YAGNA_REST_URL.substitute(host="0.0.0.0"),
         "RUST_BACKTRACE": "1",
