ec77363cb3b64cb38cb83a98505e814a988ea699
Kuba Mazurek <zakaprov@gmail.com>
2020-09-11 18:48:40 +0200

    Use fixed path as project root

--- goth/project.py
+++ goth/project.py
@@ -1,28 +1,8 @@
 """Module with information on the project itself, e.g. project root directory."""
 from pathlib import Path
-from typing import Optional
 
 import goth
 
 
-def _find_parent_with(start: Path, child_name: str) -> Optional[Path]:
-    """Walk filesystem upwards from `start` looking for dir containing `child_name`.
-
-    Return first dir with item named `child_name` or None if no matches were found.
-    """
-    parents = start.resolve().parents
-    to_visit = [start] + list(parents)
-
-    for current in to_visit:
-        current = current.resolve()
-        for child in current.iterdir():
-            if child.name == child_name:
-                return current
-
-    return None
-
-
-PROJECT_ROOT = _find_parent_with(Path(goth.__file__).parent, ".git")
-if not PROJECT_ROOT:
-    raise RuntimeError("Failed to find project root.")
+PROJECT_ROOT = Path(goth.__file__).parent.parent
 DOCKER_DIR = PROJECT_ROOT / "docker"
