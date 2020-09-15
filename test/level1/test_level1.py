03caed301ddf5f7443a93a4f19699bab1022f7d5
Kuba Mazurek <zakaprov@gmail.com>
2020-09-15 15:23:46 +0200

    Remove sequence runner

--- test/level1/test_level1.py
+++ test/level1/test_level1.py
@@ -14,11 +14,10 @@ from goth.address import (
 )
 from goth.node import node_environment, VOLUMES
 from goth.runner.container.yagna import YagnaContainerConfig
-from goth.runner.probe import ProviderProbe, Provider, Requestor
+from goth.runner.probe import Provider, Requestor
 from goth.runner.provider import ProviderProbeWithLogSteps
-from goth.runner.requestor import ApiEnabledRequestorProbe, RequestorProbeWithApiSteps
+from goth.runner.requestor import RequestorProbeWithApiSteps
 from goth.runner.simple import SimpleRunner
-from goth.runner.sequence import ProbeStepBuilder, SequenceRunner
 
 logger = logging.getLogger(__name__)
 
@@ -26,15 +25,14 @@ logger = logging.getLogger(__name__)
 LEVEL1_TOPOLOGY = [
     YagnaContainerConfig(
         name="requestor",
-        # TODO: `role` should be replaced by something like `probe_class`
-        role=ApiEnabledRequestorProbe,
+        role=RequestorProbeWithApiSteps,
         environment=node_environment(account_list="/asset/key/001-accounts.json"),
         key_file="/asset/key/001.json",
         volumes=VOLUMES,
     ),
     YagnaContainerConfig(
         name="provider_1",
-        role=ProviderProbe,
+        role=ProviderProbeWithLogSteps,
         # Configure this provider node to communicate via proxy
         environment=node_environment(
             market_url_base=MARKET_BASE_URL.substitute(host=PROXY_HOST),
@@ -44,7 +42,7 @@ LEVEL1_TOPOLOGY = [
     ),
     YagnaContainerConfig(
         name="provider_2",
-        role=ProviderProbe,
+        role=ProviderProbeWithLogSteps,
         # Configure the second provider node to communicate via proxy
         environment=node_environment(
             market_url_base=MARKET_BASE_URL.substitute(host=PROXY_HOST),
@@ -56,79 +54,8 @@ LEVEL1_TOPOLOGY = [
 
 
 @pytest.mark.asyncio
-async def test_level1_steps(logs_path: Path, assets_path: Optional[Path]):
-    """Test running Level1Scenario."""
-
-    runner = SequenceRunner(
-        LEVEL1_TOPOLOGY, "assertions.level1_assertions", logs_path, assets_path
-    )
-
-    providers: ProbeStepBuilder = runner.get_builder(role=Provider)
-    requestor: ProbeStepBuilder = runner.get_builder(role=Requestor)
-
-    # Market
-    providers.wait_for_offer_subscribed()
-    subscription_id = requestor.subscribe_demand()
-    proposals = requestor.wait_for_proposals(subscription_id, providers._probes)
-    requestor.counter_proposals(subscription_id, proposals)
-    providers.wait_for_proposal_accepted()
-    requestor.wait_for_proposals(subscription_id, providers._probes)
-    agreement_ids = requestor.create_agreements(proposals)
-    requestor.confirm_agreements(agreement_ids)
-    providers.wait_for_agreement_approved()
-    requestor.unsubscribe_demand(subscription_id)
-
-    # Activity
-    activity_ids = requestor.create_activities(agreement_ids)
-    providers.wait_for_activity_created()
-    activity_batches = requestor.call_exec(activity_ids)
-    providers.wait_for_exeunit_started()
-    requestor.collect_results(activity_batches)
-    requestor.destroy_activities(activity_ids)
-    providers.wait_for_exeunit_finished()
-
-    # Payment
-    providers.wait_for_invoice_sent()
-    invoices = requestor.gather_invoices(agreement_ids)
-    requestor.pay_invoices(invoices)
-    providers.wait_for_invoice_paid()
-
-    await runner.run_scenario()
-
-
-@pytest.mark.asyncio
-async def test_level1_simple(logs_path: Path, assets_path: Optional[Path]):
+async def test_level1(logs_path: Path, assets_path: Optional[Path]):
     """Test running Level1Scenario with SimpleRunner."""
-
-    topology = [
-        YagnaContainerConfig(
-            name="requestor",
-            role=RequestorProbeWithApiSteps,
-            environment=node_environment(account_list="/asset/key/001-accounts.json"),
-            key_file="/asset/key/001.json",
-            volumes=VOLUMES,
-        ),
-        YagnaContainerConfig(
-            name="provider_1",
-            role=ProviderProbeWithLogSteps,
-            # Configure this provider node to communicate via proxy
-            environment=node_environment(
-                market_url_base=MARKET_BASE_URL.substitute(host=PROXY_HOST),
-                rest_api_url_base=YAGNA_REST_URL.substitute(host=PROXY_HOST),
-            ),
-            volumes=VOLUMES,
-        ),
-        YagnaContainerConfig(
-            name="provider_2",
-            role=ProviderProbeWithLogSteps,
-            # Configure the second provider node to communicate via proxy
-            environment=node_environment(
-                market_url_base=MARKET_BASE_URL.substitute(host=PROXY_HOST),
-                rest_api_url_base=YAGNA_REST_URL.substitute(host=PROXY_HOST),
-            ),
-            volumes=VOLUMES,
-        ),
-    ]
 
     # TODO: provide the exe script in a fixture?
     if assets_path is None:
@@ -139,10 +66,10 @@ async def test_level1_simple(logs_path: Path, assets_path: Optional[Path]):
     exe_script = exe_script_path.read_text()
 
     async with SimpleRunner(
-        topology, "assertions.level1_assertions", logs_path, assets_path
+        LEVEL1_TOPOLOGY, "assertions.level1_assertions", logs_path, assets_path
     ) as runner:
 
-        requestor = runner.get_probes(name="requestor")[0]
+        requestor = runner.get_probes(role=Requestor)[0]
         assert isinstance(requestor, RequestorProbeWithApiSteps)
 
         provider_1, provider_2 = runner.get_probes(role=Provider)
