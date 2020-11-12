f16bd2542300e9182b287b8b5029324c1cca5073
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-11-12 20:48:22 +0100

    docstring

--- scripts/eth_key.py
+++ scripts/eth_key.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script to convert raw hex private keys to keystore files.
+
+- `raw hex private keys` are found in the ganache logs ( ethereum.log )
+- `keystore files` are used to import identities into yagna
+"""
+
 from eth_keyfile import create_keyfile_json
 import json
 import sys
