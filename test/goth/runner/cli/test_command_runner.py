13444d07f09a7b2d4ad6cdf97dcb121e5f468815
Kuba Mazurek <zakaprov@gmail.com>
2020-12-11 17:17:19 +0100

    Use DockerCommandRunner in unit tests

--- test/goth/runner/cli/test_command_runner.py
+++ test/goth/runner/cli/test_command_runner.py
@@ -1,16 +1,15 @@
 """Tests for `runner.cli.base`.
 
 Containing:
+- `runner.cli.base.CommandRunner`
 - `runner.cli.base.DockerCommandRunner`
-- `runner.cli.base.DockerJSONCommandRunner`
 """
 import shlex
 
 import pytest
 from unittest.mock import patch
 
-from goth.runner.cli import DockerJSONCommandRunner
-from goth.runner.cli.base import CommandRunner
+from goth.runner.cli.base import CommandRunner, DockerCommandRunner
 from goth.runner.exceptions import CommandError
 
 parameter_names = "command, expected_stdout, expected_stderr"
@@ -25,6 +24,7 @@ parameter_values = [
 @patch("goth.runner.cli.base.logger")
 def test_runner_output_demuxing(_logger, command, expected_stdout, expected_stderr):
     """Test that stdout/stderr from a command are demultiplexed."""
+
     runner = CommandRunner("/bin/sh")
 
     stdout, stderr = runner.run_command("-c", command)
@@ -52,7 +52,7 @@ def test_json_runner_output_demuxing(
 ):
     """Test that stdout/stderr from a command are demultiplexed."""
 
-    runner = DockerJSONCommandRunner(yagna_container, "/bin/sh")
+    runner = DockerCommandRunner(yagna_container, "/bin/sh")
 
     stdout, stderr = runner.run_command("-c", shlex.quote(command))
     assert stdout == expected_stdout
@@ -63,7 +63,7 @@ def test_json_runner_output_demuxing(
 def test_json_runner_error_raising(_logger, yagna_container):
     """Test that the correct error is raised when command exits with non-zero code."""
 
-    runner = DockerJSONCommandRunner(yagna_container, "/bin/sh")
+    runner = DockerCommandRunner(yagna_container, "/bin/sh")
 
     with pytest.raises(CommandError) as ce:
         runner.run_command("-c", "'echo STDOUT; no-such-command'")
