fd67f97576979ba39878fee6d358dff1f1d57fde
azawlocki <artur.zawlocki@golem.network>
2021-08-13 15:11:21 +0200

    Enable proxy for provider agent Market API calls

--- goth/node.py
+++ goth/node.py
@@ -4,6 +4,7 @@ from typing import Dict
 
 from goth.address import (
     ACTIVITY_API_URL,
+    MARKET_API_URL,
     PAYMENT_API_URL,
     ROUTER_HOST,
     ROUTER_PORT,
@@ -45,7 +46,12 @@ def node_environment(
 
     if rest_api_url_base:
         agent_env = {
+            # Setting URLs for all three APIs has the same effect as setting
+            # YAGNA_API_URL to YAGNA_REST_URL.substitute(base=rest_api_url_base).
+            # We set all three separately so it's easier to selectively disable
+            # proxy for certain APIs (if a need to do so arises).
             "YAGNA_ACTIVITY_URL": ACTIVITY_API_URL.substitute(base=rest_api_url_base),
+            "YAGNA_MARKET_URL": MARKET_API_URL.substitute(base=rest_api_url_base),
             "YAGNA_PAYMENT_URL": PAYMENT_API_URL.substitute(base=rest_api_url_base),
         }
         node_env.update(agent_env)
