cd206aaf0124e1d227b7615a39ddb157dea5aa55
azawlocki <artur.zawlocki@golem.network>
2021-08-31 23:58:49 +0200

    Changes in logging configuration (separate loggers MITM proxy addons)

--- goth/runner/log.py
+++ goth/runner/log.py
@@ -67,14 +67,14 @@ LOGGING_CONFIG = {
         "goth.api_monitor": {
             "handlers": [],
             "propagate": False,
-            # Setting this to "DEBUG" can help in diagnosing issues with routing
-            # in the proxy. Use "INFO" to avoid verbose logging of requests/responses.
             "level": "DEBUG",
         },
-        "test.yagna": {
-            "handlers": ["console", "runner_file"],
+        "goth.api_monitor.router_addon": {
+            "handlers": [],
             "propagate": False,
-            "level": "DEBUG",
+            # Setting this to "DEBUG" can help in diagnosing issues with routing
+            # in the proxy. Use "INFO" to avoid verbose logging of requests/responses.
+            "level": "INFO",
         },
         "aiohttp": {
             "handlers": ["console", "runner_file"],
@@ -82,6 +82,10 @@ LOGGING_CONFIG = {
             "propagate": False,
         },
         "transitions": {"level": "WARNING"},
+        "root": {
+            "handlers": ["console", "runner_file"],
+            "level": "INFO",
+        },
     },
 }
 
