d22cf4c0299fa6f39be3663549f0569105142865
Jan Betley <jan.betley@golem.network>
2021-10-28 16:15:10 +0200

    payment_config cleanup

--- goth/payment_config.py
+++ goth/payment_config.py
@@ -1,32 +1,9 @@
+"""All possible payment-related configuration, in one place."""
 from dataclasses import dataclass
 from typing import Dict
 
-# def payments_env(payments: str) -> Dict[str, str]:
-#     """Payment-related part of the environment."""
-# 
-#     contract_address = "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34"
-#     ethereum_container_url = "http://ethereum:8545"
-#     payments_env = {
-#         'polygon': {
-#             "YA_PAYMENT_NETWORK": "polygon",
-#             "POLYGON_GETH_ADDR": ethereum_container_url,
-#             "POLYGON_GLM_CONTRACT_ADDRESS": contract_address,
-#         },
-#         'mainnet': {
-#             "YA_PAYMENT_NETWORK": "mainnet",
-#             "MAINNET_GETH_ADDR": ethereum_container_url,
-#             "MAINNET_GLM_CONTRACT_ADDRESS": contract_address,
-#         },
-#         'zksync': {
-#             #   TODO: For some reason, "rinkeby" works and "mainnet" doesn't. Why?
-#             "YA_PAYMENT_NETWORK": "rinkeby",
-#             "ZKSYNC_FAUCET_ADDR": "http://zksync:3030/zk/donatex",
-#             "ZKSYNC_RINKEBY_RPC_ADDRESS": "http://zksync:3030",
-#         },
-#     }
-#     return payments_env[payments]
-
-
+GETH_ADDR = "http://ethereum:8545"
+GLM_CONTRACT_ADDRESS = "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34"
 
 _payment_config = {
     'zksync': {
@@ -42,28 +19,41 @@ _payment_config = {
     'erc20': {
         'env': {
             "YA_PAYMENT_NETWORK": "mainnet",
-            "MAINNET_GETH_ADDR": "http://ethereum:8545",
-            "MAINNET_GLM_CONTRACT_ADDRESS": "0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34",
+            "MAINNET_GETH_ADDR": GETH_ADDR,
+            "MAINNET_GLM_CONTRACT_ADDRESS": GLM_CONTRACT_ADDRESS,
         },
         'driver': 'erc20',
         'network': 'mainnet',
         'token': 'GLM',
     },
+    'polygon': {
+        'env': {
+            "YA_PAYMENT_NETWORK": "polygon",
+            "MAINNET_GETH_ADDR": GETH_ADDR,
+            "MAINNET_GLM_CONTRACT_ADDRESS": GLM_CONTRACT_ADDRESS,
+        },
+        'driver': 'erc20',
+        'network': 'polygon',
+        'token': 'GLM',
+    }
 }
 
 
 @dataclass
 class PaymentConfig:
+    """All payment-related config for a single container."""
+
     env: Dict[str, str]
     driver: str
     network: str
     token: str
 
 
-def get_payment_config(name: str) -> PaymentConfig:
+def get_payment_config(config_payments_name: str) -> PaymentConfig:
+    """Translate "payments" name from goth-config.yml to a PaymentConfig instance."""
     try:
-        payment_config_kwargs = _payment_config[name]
+        payment_config_kwargs = _payment_config[config_payments_name]
     except KeyError:
-        raise KeyError(f"Unknown payment config name: {name}")
+        raise KeyError(f"Unknown payment config name: {config_payments_name}")
 
     return PaymentConfig(**payment_config_kwargs)
