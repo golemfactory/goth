7bd403f08a9af71ad626ec31f46063122cfaa32a
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2023-07-05 18:25:14 +0200

    remove additional logging

--- goth/runner/probe/mixin.py
+++ goth/runner/probe/mixin.py
@@ -138,7 +138,6 @@ class MarketApiMixin:
     async def subscribe_demand(self: ProbeProtocol, demand: Demand) -> Tuple[str, Demand]:
         """Call subscribe demand on the market api."""
         subscription_id = await self.api.market.subscribe_demand(demand)
-        logger.debug("Subscribe demand: %s.", demand)
         return subscription_id, demand
 
     @step()
@@ -159,8 +158,6 @@ class MarketApiMixin:
             },
             constraints=constraints,
         )
-
-        logger.debug("Subscribe template demand.")
 
         return await self.subscribe_demand(demand)  # type: ignore
 
