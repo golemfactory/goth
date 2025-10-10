074d7304fa7bbacf1e35d32afa80e8b76bd5cba5
scx1332 <sieciech@gmail.com>
2025-10-10 17:53:15 +0200

    fix timezone

--- goth/__main__.py
+++ goth/__main__.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 def make_logs_dir(base_dir: Path) -> Path:
     """Create a unique subdirectory for this test run."""
 
-    date_str = datetime.now(tz=timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
+    date_str = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
     log_dir = base_dir / f"goth_{date_str}"
     log_dir.mkdir(parents=True)
 
